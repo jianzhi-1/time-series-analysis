14e37ab7ebf31a62ca593ac3df36020919ab8f48
Jianzhi Wang <jianzhiw00@gmail.com>
2023-08-05 16:55:12 -0500

    Update cross-correlation.py

--- cross-correlation.py
+++ cross-correlation.py
@@ -1,4 +1,4 @@
-def E(x, y, lag=0): # given two series x, y, calculate E[XY] (reflective of Pearson's coefficient since E[X], E[Y], sigma_X, sigma_Y assumed to be fixed
+def E(x, y, lag=0, lim=10): # given two series x, y, calculate E[XY] (reflective of Pearson's coefficient since E[X], E[Y], sigma_X, sigma_Y assumed to be fixed
     assert len(x) == len(y)
     xlag, ylag = None, None
     if lag >= 0:
@@ -8,10 +8,12 @@ def E(x, y, lag=0): # given two series x, y, calculate E[XY] (reflective of Pear
         ylag = y[-lag:]
         xlag = x[:len(x) + lag]
     assert len(xlag) == len(ylag)
-    assert len(xlag) > 10 # ensure you have enough data to get an accurate coefficient in the first place
+    assert len(xlag) > lim # ensure you have enough data to get an accurate coefficient in the first place
     return np.dot(xlag, ylag)/len(xlag)
 
-lag_arr = np.arange(-40, 40, 1)
-res = [E(x, y, lag) for lag in lag_arr] 
-# arg max argument denotes the lag that corresponds to the highest correlation
-# arg min argument denotes the lag that corresponds to the highest anti-correlation
+def plotE(x, y, maxlag=40, lim=10):
+    lag_arr = np.arange(-maxlag, maxlag, 1)
+    res = [E(x, y, lag=lag, lim=10) for lag in lag_arr]
+    plt.plot(lag_arr, res, label="E[XY]")
+    plt.legend()
+    plt.show()
